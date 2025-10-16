00db01e977dd28de87be65d2bde480f5f2f70d2f
zainahsan92 <zainahsan660@gmail.com>
2025-10-16 09:59:36 +0500

    cors issue fixing

--- sabac_project/settings.py
+++ sabac_project/settings.py
@@ -42,8 +42,15 @@ DEBUG = True
 
 
 
-ALLOWED_HOSTS = ["*", "sabac-server-530056698.us-central1.run.app","sabac-webapp-382170497486.us-central1.run.app"]
-CSRF_TRUSTED_ORIGINS = ["https://*"]
+ALLOWED_HOSTS = ["*", "sabac-server-530056698.us-central1.run.app",
+                "sabac-webapp-382170497486.us-central1.run.app"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://sabac-server-530056698.us-central1.run.app",
+    "https://sabac-webapp-382170497486.us-central1.run.app",
+]
+
+# CSRF_TRUSTED_ORIGINS = ["https://*"]
 
 # ALLOWED_HOSTS = [
 #     "sabac-server-530056698.us-central1.run.app",
